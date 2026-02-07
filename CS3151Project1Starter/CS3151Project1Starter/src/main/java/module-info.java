940d0160487718bf7b6441e751a15a12e2a3b398
Kenneth Dearman <164975838+kd00250@users.noreply.github.com>
2026-02-07 12:49:35 -0500

    Delete CS3151Project1Starter/CS3151Project1Starter directory

--- CS3151Project1Starter/CS3151Project1Starter/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module edu.westga.dsdm.knightstravails {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens edu.westga.dsdm.knightstravails to javafx.fxml;
-    exports edu.westga.dsdm.knightstravails;
-    exports edu.westga.dsdm.knightstravails.view;
-    opens edu.westga.dsdm.knightstravails.view to javafx.fxml;
-}
