29e913ac7ea66eb9943f4db79017e7731a126097
Kenneth Dearman <164975838+kd00250@users.noreply.github.com>
2026-02-08 20:16:28 -0500

    Working through the solve
    Stopped to watch super bowl halftime show, still not done with the knight transversal solver

--- CS3151Project1KennethDearman/CS3151Project1KennethDearman/src/main/java/edu/westga/dsdm/knightstravails/solver/SolvePuzzle.java
+++ CS3151Project1KennethDearman/CS3151Project1KennethDearman/src/main/java/edu/westga/dsdm/knightstravails/solver/SolvePuzzle.java
@@ -2,7 +2,10 @@ package edu.westga.dsdm.knightstravails.solver;
 
 import edu.westga.dsdm.knightstravails.model.Position;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * The Solve Puzzle Class
@@ -26,7 +29,41 @@ public class SolvePuzzle {
         if (currentPos == null || endPosition == null) {
             throw new IllegalArgumentException("Either start or end position cannot be null.");
         }
-        
+        Queue<Position> nextNodes = new LinkedList<>();
+        HashMap<Position, Position> predecesor = new HashMap<>();
+        HashSet<Position> selected = new HashSet<>();
+
+        nextNodes.offer(currentPos);
+        selected.add(currentPos);
+        predecesor.put(currentPos, null);
+
+        while (!nextNodes.isEmpty()) {
+            Position spaceOnBoard = nextNodes.poll();
+
+            if (spaceOnBoard.equals(endPosition)) {
+                this.solutionPath = reconstructPath(predecesor, endPosition);
+                return this.solutionPath;
+            }
+
+            for (int[] move : KNIGHT_MOVES) {
+                int newRow = spaceOnBoard.row() + move[0];
+                int newCol = spaceOnBoard.col() + move[1];
+
+                if (this.isValid(newRow, newCol) {
+
+                }
+            }
+        }
         return null;
+    }
+
+    /**
+     * Checks if the neighbor move is valid or not
+     * @param row the row of the neighbor
+     * @param col the col of the neighbor
+     * @return true or false if the move is valid
+     */
+    private boolean isValid(int row, int col) {
+        return row >= 0 && row < Position.MAX_ROWS && col >= 0 && col < Position.MAX_COLS;
     }
 }
